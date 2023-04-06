08e711074fb523b079bc1cf6ba24990592a6c9fd
Ashik K <root@ashik.se>
2023-04-06 17:02:31 +0200

    wip2: title generation with OpenAI

--- titlegen.py
+++ titlegen.py
@@ -2,21 +2,22 @@ import sys
 import csv
 import openai
 
-def main():
-    # check if the user supplied the correct number of command line arguments
-    if len(sys.argv) != 3:
-        print("Usage: python titlegen.py filename.csv API_key")
-        sys.exit(1)
-    #set the api key from the command line argument
-    openai.api_key = sys.argv[2]
-    with open(sys.argv[1], "r") as file:
+# check if the user supplied the correct number of command line arguments
+if len(sys.argv) != 4:
+    print("Usage: python titlegen.py inputfilename.csv outputfilename.csv API_key")
+    sys.exit(1)
+    # set the api key from the command line argument
+openai.api_key = sys.argv[3]
+with open(sys.argv[1], "r") as file:
+        outfile = open(sys.argv[2], "w")
+        writer = csv.writer(outfile)
         reader = csv.reader(file)
         for row in reader:
             print(row[4])
             # summarize the text in the 5th column using OpenAi's GPT-3
-            # create a variable called prompt and set it as the concatenatenation of the string "Summarize this 
+            # create a variable called prompt and set it as the concatenatenation of the string "Summarize this
             # in one sentence:" and the text in the 5th column
-            prompt = "Summarize this in one sentence:" + row[4]
+            prompt = "Summarize this in one English sentence of not more than 4 words:" + row[4]
 
             response = openai.Completion.create(
                 model="gpt-3.5-turbo",
@@ -30,6 +31,8 @@ def main():
             # write the response as the last column in each row of the same csv file
             row.append(response)
             print(row)
-        
+            # write the row to the csv file
 
-
+            writer.writerow(row)
+            outfile.close()
+            file.close()
